beb62ff47dce26a22358276727879acb5468153d
Batool Malkawi <batoolmalkawii@gmail.com>
2020-10-07 11:52:08 +0300

    after styling+JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let totalPrice = 0;
+let existingLocalStorage = [];
 function Device(name, category, quantity) {
   this.name = name;
   this.category = category;
+  this.price = getRandNum(350, 750);
   this.quantity = quantity;
   Device.all.push(this);
 }
@@ -12,11 +14,70 @@ Device.all = [];
 let formEl = document.getElementById('form');
 formEl.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event){
+function handleSubmit(event) {
   event.preventDefault();
   let name = event.target.name.value;
   let category = event.target.category.value;
   let quantity = event.target.quantity.value;
   let device = new Device(name, category, quantity);
-  
+  saveToLocalStorage(device);
+  render();
 }
+
+function saveToLocalStorage(device) {
+  existingLocalStorage.push(device);
+  localStorage.setItem('Devices', JSON.stringify(existingLocalStorage));
+}
+
+function getFromLocalStorage() {
+  existingLocalStorage = JSON.parse(localStorage.getItem('Devices')) || [];
+}
+
+function render() {
+  formEl.reset();
+  let sectionEl = document.getElementById('tableSection');
+  sectionEl.textContent = '';
+  getFromLocalStorage();
+  let tableEl = document.createElement('table');
+  sectionEl.appendChild(tableEl);
+  let thElName = document.createElement('th');
+  tableEl.appendChild(thElName);
+  thElName.textContent = 'Device Name';
+  let thElQuantity = document.createElement('th');
+  tableEl.appendChild(thElQuantity);
+  thElQuantity.textContent = 'Quantity';
+  let thElPrice = document.createElement('th');
+  tableEl.appendChild(thElPrice);
+  thElPrice.textContent = 'Unit Price';
+  let thElCategory= document.createElement('th');
+  tableEl.appendChild(thElCategory);
+  thElCategory.textContent = 'Category';
+  for (let i = 0; i < existingLocalStorage.length; i++) {
+    let trEl = document.createElement('tr');
+    tableEl.appendChild(trEl);
+    let tdElName = document.createElement('td');
+    trEl.appendChild(tdElName);
+    tdElName.textContent = existingLocalStorage[i].name;
+    let tdElQuantity = document.createElement('td');
+    trEl.appendChild(tdElQuantity);
+    tdElQuantity.textContent = existingLocalStorage[i].quantity;
+    let tdElPrice = document.createElement('td');
+    trEl.appendChild(tdElPrice);
+    tdElPrice.textContent = existingLocalStorage[i].price;
+    let devicePrice = existingLocalStorage[i].quantity * existingLocalStorage[i].price;
+    totalPrice = totalPrice + devicePrice;
+    let tdElCategory = document.createElement('td');
+    trEl.appendChild(tdElCategory);
+    tdElCategory.textContent = existingLocalStorage[i].category;
+  }
+  let pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `Total: ${totalPrice}`;
+}
+
+function getRandNum(min, max) {
+  let random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return (random);
+}
+
+render();
